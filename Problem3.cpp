1902dd142950279c30d293f2e1306f511e1836ea
Rohit Kumar Singh <kumar.rohit.rk@gmail.com>
2021-03-07 00:48:44 +0530

    problem 3

---
+++ Problem3.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lengthOfLongestSubstring(string s) {
+        vector<int> hashTable(256,0);
+        int l =0,r =0,maxlen = 0;
+        cout <<"String length" << s.length()<< " r is "<<r<< (r < s.length())<<endl;
+        while ((l <= r) && (r < s.length())){   
+            cout << r << " " << s[r] << " " << hashTable[s[r]]<< endl;
+            if (hashTable[s[r]] == 0){
+                hashTable[s[r]]++;
+                r++;
+                maxlen = ((r-l)>maxlen) ? r-l : maxlen;
+            }else{
+               hashTable[s[l]]--;
+                l++;
+            }
+        }
+        return maxlen;
+    }
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int res = lengthOfLongestSubstring("bbbbb");
+    cout<<"Max len is "<<res<<endl;
+    return 0;
+}
