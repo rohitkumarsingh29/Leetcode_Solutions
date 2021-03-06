0aee9e77f38d8dd052aa48f96e79f952cc20291b
Rohit Kumar Singh <kumar.rohit.rk@gmail.com>
2021-03-07 00:49:10 +0530

    problem 3

--- Problem3.cpp
+++ Problem3.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int lengthOfLongestSubstring(string s) {
         vector<int> hashTable(256,0);
         int l =0,r =0,maxlen = 0;
-        cout <<"String length" << s.length()<< " r is "<<r<< (r < s.length())<<endl;
+        //cout <<"String length" << s.length()<< " r is "<<r<< (r < s.length())<<endl;
         while ((l <= r) && (r < s.length())){   
-            cout << r << " " << s[r] << " " << hashTable[s[r]]<< endl;
+            //cout << r << " " << s[r] << " " << hashTable[s[r]]<< endl;
             if (hashTable[s[r]] == 0){
                 hashTable[s[r]]++;
                 r++;
